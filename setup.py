7d412a4a30c9d2d9d087ad4fb50d6c5e5f153a48
Valtteri Kortesmaa <mulderns@iki.fi>
2021-02-03 11:49:40 +0200

    update version info

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.18',
+    version='0.1.1',
 
     description='Tools for debugging Django, pprint models',
     long_description=long_description,
@@ -48,9 +48,9 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         #'Programming Language :: Python :: 3.3',
         #'Programming Language :: Python :: 3.4',
         #'Programming Language :: Python :: 3.5',
